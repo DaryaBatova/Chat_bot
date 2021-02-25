5750cd44434463dafc6404d9e0aa6860a7c36665
Darya Batova <darmorgan99@gmail.com>
2021-02-25 17:31:13 +0300

    Добавлена структура сценариев. Доработана логика бота.

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 import logging
+import handlers
 
 try:
     import settings
@@ -13,7 +14,12 @@ log = logging.getLogger('bot')
 
 def configure_logging():
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+    stream_handler.setFormatter(
+        logging.Formatter(
+            '%(asctime)s %(levelname)s %(message)s',
+            datefmt='%d-%m-%y %H:%M'
+        )
+    )
     stream_handler.setLevel(logging.INFO)
     log.addHandler(stream_handler)
 
@@ -28,11 +34,24 @@ def configure_logging():
     log.setLevel(logging.DEBUG)
 
 
+class UserState:
+    """ Состояние пользователя внутри сценария. """
+    def __init__(self, scenario_name, step_name, context=None):
+        self.scenario_name = scenario_name
+        self.step_name = step_name
+        self.context = context or dict()
+
+
 class Bot:
     """
-    Echo bot для vk.com
-
+    Сценарий регистрации на конференцию через vk.com.
     Use python3.9
+
+    Поддерживает ответы на вопросы про дату, место проведения и сценарий регистрации:
+    - спрашиваем имя
+    - спрашиваем email
+    - сообщаем об успешной регистрации
+    Если шаг не пройден, задаем уточняющий вопрос до тех пор, пока шаг не будет пройден.
     """
     def __init__(self, group_id, token):
         """ Конструктор класса.
@@ -45,6 +64,7 @@ class Bot:
         self.vk = vk_api.VkApi(token=token)
         self.long_poller = VkBotLongPoll(self.vk, self.group_id)
         self.api = self.vk.get_api()
+        self.user_states = dict()  # user_id -> UserState
 
     def run(self):
         """ Запуск echo бота. """
@@ -55,31 +75,82 @@ class Bot:
                 log.exception('Error in the processing of the event')
 
     def on_event(self, event):
-        """ Отправляет сообщение назад, если сообщение представляет собой текст.
+        """ Отправляет сообщение-ответ на запрос пользователя.
 
         :param event: VkBotMessageEvent object
         :return: None
         """
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            log.debug('Sending message')
-            message = 'Что ты хочешь на обед?'
-            if event.object.text.lower() != 'борщ':
-                self.api.messages.send(
-                    message=message,
-                    random_id=random.randint(0, 2 ** 20),
-                    peer_id=event.object.peer_id
-                )
-            else:
-                message = 'Круто! Пойду готовить'
-                self.api.messages.send(
-                    message=message,
-                    random_id=random.randint(0, 2 ** 20),
-                    peer_id=event.object.peer_id
-                )
-        else:
+        if event.type != VkBotEventType.MESSAGE_NEW:
             log.info('Пришло сообщение типа %s', event.type)
-            raise ValueError('Unknown message')
-            # print(f'Пришло сообщение типа {event.type}')
+            return
+
+        # определяем id пользователя и текст сообщения
+        user_id = event.object.peer_id
+        text = event.object.text
+
+        # находится ли пользователь в некотором сценарии
+        if user_id in self.user_states:
+            # продолжаем сценарий
+            text_to_send = self.continue_scenario(user_id, text)
+        else:
+            # определяем потребность пользователя
+            for intent in settings.INTENTS:
+                log.debug(f'User gets {intent}')
+
+                if any(token in text.lower() for token in intent['tokens']):
+                    if intent['answer']:
+                        text_to_send = intent['answer']
+                    else:
+                        text_to_send = self.start_scenario(user_id, intent['scenario'])
+                    break
+            else:
+                text_to_send = settings.DEFAULT_ANSWER
+
+        self.api.messages.send(
+            message=text_to_send,
+            random_id=random.randint(0, 2 ** 20),
+            peer_id=user_id
+        )
+
+    def start_scenario(self, user_id, scenario_name):
+        """Начать сценарий для пользователя.
+
+        :param user_id: id пользователя
+        :param scenario_name: имя соответствуюшего сценария
+        :return: сообщение, которое необходимо отправить пользователю.
+        """
+        scenario = settings.SCENARIOS[scenario_name]
+        first_step = scenario['first_step']
+        step = scenario['steps'][first_step]
+        text_to_send = step['text']
+        self.user_states[user_id] = UserState(scenario_name=scenario_name, step_name=first_step)
+        return text_to_send
+
+    def continue_scenario(self, user_id, text):
+        """Продолжить сценарий для пользователя.
+
+        :param user_id: id пользователя
+        :param text: сообщение пользователя
+        :return: сообщение, которое необходимо отправить пользователю.
+        """
+        state = self.user_states[user_id]
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        step = steps[state.step_name]
+
+        handler = getattr(handlers, step['handler'])
+        if handler(text=text, context=state.context):
+            next_step = steps[step['next_step']]
+            text_to_send = next_step['text'].format(**state.context)
+            if next_step['next_step']:
+                state.step_name = step['next_step']
+            else:
+                self.user_states.pop(user_id)
+                log.info('Зарестрирован: {name} {email}'.format(**state.context))
+        else:
+            # повторить шаг текущего сценария
+            text_to_send = step['failure_text'].format(**state.context)
+
+        return text_to_send
 
 
 if __name__ == '__main__':
